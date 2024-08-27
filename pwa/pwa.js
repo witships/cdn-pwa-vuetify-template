261975f521670bd93e7e5554b34a541eac08d89b
witships <witships@gmail.com>
2024-08-27 10:07:11 +0900

    リファクタ

--- pwa/pwa.js
+++ pwa/pwa.js
@@ -1,22 +1,17 @@
 let deferredPrompt
-// const addBtn = document.querySelector(".add-button");
 const addBtn = document.getElementById("install-pwa")
 
+// pwaのインストールボタンを表示
 window.addEventListener("beforeinstallprompt", (e) => {
   console.log(e)
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault()
-  // Stash the event so it can be triggered later.
   deferredPrompt = e
-  // Update UI to notify the user they can add to home screen
   // ブラウザを判断し、インストールボタンを表示
   addBtn.style.display = "flex"
 
   addBtn.addEventListener("click", () => {
-    // hide our user interface that shows our A2HS button
     // インストール済みの場合は、インストールボタンを非表示
     addBtn.style.display = "none"
-    // Show the prompt
     // インストール確認ダイアログ
     deferredPrompt.prompt()
     deferredPrompt.userChoice.then((choiceResult) => {
--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 - vue3,vuetify を cdn で利用
 - pwa に対応
   - サーバー起動時のみ、index.html で PWA のインストールが可能
+  - PWA の設定は、manifest.webmanifest で設定
 - vuetify.html は、pwa 対応していない vuetify ページ
 - vanilla.html は、pwa のみ対応、vutify の無いページ
 
